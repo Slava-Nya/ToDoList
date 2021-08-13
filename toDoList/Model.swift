3e719288aeb25ecf8ebc09cb8f7e9d6628857e2d
Slava-Nya <svyatoslavank@gmail.com>
2021-08-13 23:26:45 +0300

    add tableViewController

---
+++ toDoList/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  toDoList
+//
+//  Created by Halfhand Lorrine on 8/13/21.
+//  Copyright © 2021 Halfhand Lorrine. All rights reserved.
+//
+
+import Foundation
+
+var ToDoItems: [String] = ["1", "two", "три"]
+
+
+
+
